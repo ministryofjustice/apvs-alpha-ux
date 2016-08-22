2544b81ab14f5d9eed44fc28da221d1eb753d219
Chris Sisk <me@chrisisk.com>
2016-08-22 15:44:30 +0100

    eligibility content added

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Assisted Prison Visits Scheme Alpha',
+  serviceName: 'Assisted Prison Visits Scheme',
 
   // Default port that prototype runs on
   port: '3000',
