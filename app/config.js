c468e87e177e1a8e17301eb2154e9737568484d9
Chris Sisk <me@chrisisk.com>
2016-08-22 10:55:11 +0100

    service name change and start page created

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Assisted Prison Visits Scheme Alpha',
 
   // Default port that prototype runs on
   port: '3000',
