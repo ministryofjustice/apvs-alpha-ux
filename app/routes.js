fc530fa274db9716bc9a54caf2be042d47e2fd0b
Chris Sisk <me@chrisisk.com>
2016-09-08 16:13:08 +0100

    version 2.4 created for returning users making a claim

--- app/routes.js
+++ app/routes.js
@@ -149,5 +149,45 @@ router.post('/v2-3/login', function (req, res) {
   }
 })
 
+// version 2.4 start
+
+router.post('/v2-4/apply-2', function (req, res) {
+  var relationship = req.body.relationship
+
+  if (relationship === 'Other') {
+    res.redirect('apply-3')
+  } else {
+    // close relative, partner or sole visitor
+    res.redirect('apply-22')
+  }
+})
+
+router.get('/v2-4/apply-4', function (req, res) {
+  var benefit = req.query.benefit
+
+  res.render('v2-4/apply-4', { 'benefit': benefit })
+})
+
+router.get('/v2-4/apply-5', function (req, res) {
+  var benefit = req.query.benefit
+
+  if (benefit === '' | 'None of the above') {
+    res.redirect('el-no')
+  } else {
+    res.render('v2-4/apply-5', { 'benefit': benefit })
+  }
+})
+
+
+router.post('/v2-4/login', function (req, res) {
+  var returningUser = req.body.returningUser
+
+  if (returningUser=== 'Yes') {
+    res.redirect('login')
+  } else {
+    res.redirect('apply-1')
+  }
+})
+
 
 module.exports = router
