88165de3b8306c288f5e6c47ef44f2d9773e9440
Chris Sisk <me@chrisisk.com>
2016-09-23 12:57:59 +0100

    added version 3.2

--- app/routes.js
+++ app/routes.js
@@ -350,7 +350,7 @@ router.post('/v3/apply-10', function (req, res) {
   }
 })
 
-// version 3.0 start
+// version 3.1 start
 
 router.post('/v3-1/apply-2', function (req, res) {
   var relationship = req.body.relationship
@@ -391,6 +391,56 @@ router.post('/v3-1/apply-1', function (req, res) {
 })
 
 router.post('/v3-1/apply-10', function (req, res) {
+  var eligibilityNo = req.body.eligibilityNo
+
+  if (eligibilityNo=== 'Yes') {
+    res.redirect('apply-10')
+  } else {
+    res.redirect('apply-1')
+  }
+})
+
+// version 3.2 start
+
+router.post('/v3-2/apply-2', function (req, res) {
+  var relationship = req.body.relationship
+
+  if (relationship === 'Other') {
+    res.redirect('apply-3')
+  } else {
+    // close relative, partner or sole visitor
+    res.redirect('apply-22')
+  }
+})
+
+router.get('/v3-2/apply-4', function (req, res) {
+  var benefit = req.query.benefit
+
+  res.render('v3-2/apply-4', { 'benefit': benefit })
+})
+
+router.get('/v3-2/apply-5', function (req, res) {
+  var benefit = req.query.benefit
+
+  if (benefit === '' | 'None of the above') {
+    res.redirect('el-no')
+  } else {
+    res.render('v3-2/apply-5', { 'benefit': benefit })
+  }
+})
+
+
+router.post('/v3-2/apply-1', function (req, res) {
+  var returningUser = req.body.returningUser
+
+  if (returningUser=== 'Yes') {
+    res.redirect('apply-1')
+  } else {
+    res.redirect('login')
+  }
+})
+
+router.post('/v3-2/apply-10', function (req, res) {
   var eligibilityNo = req.body.eligibilityNo
 
   if (eligibilityNo=== 'Yes') {
