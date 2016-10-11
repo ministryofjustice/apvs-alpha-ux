c58c4cb7e62a812efa91190d198c19f99d76f118
Chris Sisk <me@chrisisk.com>
2016-10-11 14:49:14 +0100

    beta prototype structure added

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Assisted Prison Visits Scheme',
+  serviceName: 'Assisted Prison Visits Service',
 
   // Default port that prototype runs on
   port: '3000',
