76829de1fe73cb7883e6244e5dbab80f266f6a65
hvekriya <hareshv@nautilus.local>
2016-10-28 17:15:33 +0100

    new charts

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -1,96 +1,100 @@
-google.charts.load('current', {packages:['corechart', 'line']});
-google.charts.setOnLoadCallback(drawBarChart);
-google.charts.setOnLoadCallback(drawLineChart);
-google.charts.setOnLoadCallback(drawPie);
+function drawAllCharts() {
+    $('.allCharts').highcharts({
+        title: {
+            text: ''
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'March', 'April', 'May',
+                'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+            ]
+        },
+        labels: {
+            items: [{
+                html: 'Auto approved VS manual claims',
+                style: {
+                    left: '50px',
+                    top: '18px',
+                    color: (Highcharts.theme && Highcharts.theme.textColor) || 'black'
+                }
+            }]
+        },
+        series: [{
+            type: 'column',
+            name: 'Auto',
+            color: '#386C98',
+            data: [3, 2, 1, 3, 4, 9, 10, 12, 13, 6, 9, 7]
+        }, {
+            type: 'column',
+            name: 'Manual',
+            color: '#424E5D',
+            data: [2, 3, 5, 7, 6, 3, 7, 10, 9, 2, 4, 5]
+        }, {
+            type: 'pie',
+            name: 'Auto VS Manual',
+            data: [{
+                name: 'Auto Approved',
+                y: 13,
+                color: '#386C98'
+            }, {
+                name: 'Manually Approved',
+                y: 23,
+                color: '#424E5D'
+            }],
+            center: [100, 80],
+            size: 100,
+            showInLegend: false,
+            dataLabels: {
+                enabled: false
+            }
+        }]
+    });
+}
 
-function drawBarChart() {
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', 'Country');
-    data.addColumn('number', 'Applications');
-    data.addRows([
-      ['Jan to Feb 2016', 70],
-      ['Feb to March 2016', 80],
-      ['March to April 2016', 95],
-      ['April to May 2016', 105],
-      ['May to June 2016', 120]
-    ]);
-
-   var options = {
-     legend: 'none',
-     animation:{
-      "startup": true,
-      duration: 1500,
-      easing: 'out',
-    },
-     bar: {
-       groupWidth: '95%'
-     },
-     vAxis: {
-       gridlines: {
-         count: 4
-       }
-      }
-   };
-
-   var chart = new google.visualization.ColumnChart(document.getElementById('number_format_chart'));
-   chart.draw(data, options);
-  }
+// line chart
 
 function drawLineChart() {
-      var data = new google.visualization.DataTable();
-      data.addColumn('number', 'X');
-      data.addColumn('number', 'Applications');
+    $('.lineChart').highcharts({
+        title: {
+            text: '',
+            x: -20 //center
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+            ]
 
-      data.addRows([
-        [0, 0],   [1, 70],  [2, 80],  [3, 95],  [4, 105],  [5, 120]
-      ]);
-
-      var options = {
+        },
+        yAxis: {
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
         legend: {
-          position: 'top',
-          alignment: 'end'
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
         },
-        animation:{
-        "startup": true,
-         duration: 1500,
-         easing: 'out',
-       },
-        hAxis: {
-          title: 'Month'
-        },
-        vAxis: {
-          title: 'Applications'
-        },
-        backgroundColor: '#ffffff'
-      };
+        series: [{
+            name: 'Auto Approved',
+            color: '#386C98',
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        }, {
+            name: 'Manually Approved',
+            color: '#424E5D',
+            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+        }]
+    });
+}
 
-      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-      chart.draw(data, options);
-    }
+// calling all functions
+drawAllCharts();
+drawLineChart();
 
-function drawPie() {
-      var data = google.visualization.arrayToDataTable([
-        ['Task', 'Hours per Day'],
-        ['Auto Approved',     11],
-        ['Manually Approved',      2]
-      ]);
-
-      var options = {
-        pieHole: 0.2,
-        animation:{
-        "startup": true,
-         duration: 1500,
-         easing: 'out',
-       }
-      };
-
-      var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-      chart.draw(data, options);
-    }
 // for responsiv chart
-$(window).resize(function(){
-  drawBarChart();
-  drawLineChart();
-  drawPie();
+$(window).resize(function() {
+    drawAllCharts();
+    drawLineChart();
 });
