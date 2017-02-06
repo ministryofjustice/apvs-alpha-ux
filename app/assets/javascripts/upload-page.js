bcfc0f0a1e28a25fa40110b266ad64cbff1a1b39
Paul Wright <p.wright@kainos.com>
2017-02-06 17:04:23 +0000

    Add CI version 1.0 with file upload preview

---
+++ app/assets/javascripts/upload-page.js
@@ -0,0 +1,40 @@
+$('#document').change(function () {
+  var document = $('#document').val().replace(/\\/g, '/').replace(/.*\//, '')
+  if (document) {
+    $('#alternative').hide()
+    $('#document-name').html(document).addClass('text-success')
+    $('#choose-file').hide()
+    $('#label').hide()
+    $('#remove-file-upload').show()
+    previewFile()
+  }
+})
+
+function previewFile () {
+  var file = document.querySelector('input[type=file]').files[0]
+  var reader = new FileReader()
+
+  reader.addEventListener('load', function () {
+    var readerResult = reader.result
+    var fileTypeStart = readerResult.indexOf(':') + 1
+    var fileTypeEnd = readerResult.indexOf('/')
+    var fileType = readerResult.substring(fileTypeStart, fileTypeEnd)
+    if (fileType === 'image') {
+      var filePreview = document.getElementById('document-preview')
+      filePreview.src = reader.result
+      filePreview.style.display = 'block'
+      filePreview.style.visibility = 'visible'
+
+      var filePreviewTextElements = document.getElementsByClassName('document-preview-text')
+      for (var i = 0; i < filePreviewTextElements.length; i++) {
+        var textElement = filePreviewTextElements[i]
+        textElement.style.display = 'block'
+        textElement.style.visibility = 'visible'
+      }
+    }
+  }, false)
+
+  if (file) {
+    reader.readAsDataURL(file)
+  }
+}
--- app/routes.js
+++ app/routes.js
@@ -1223,4 +1223,27 @@ router.get('/beta/v6/returning.html', function (req, res) {
   }
 })
 
+var documentTypeEnum = {
+  'VISIT_CONFIRMATION': {documentType: 'VISIT-CONFIRMATION', title: 'Add visit confirmation form', guidingText: 'Provide the stamped form as evidence of your visit.'},
+  'working-tax-credit': {documentType: 'working-tax-credit', title: 'Working Tax credits or Child tax credit with disability documentation', guidingText: 'You must provide evidence of each page of your Working Tax credits or Child tax credit with disability documentation, upload one page at a time'},
+  'pension-credit': {documentType: 'pension-credit', title: 'Pension credit documentation', guidingText: 'You must provide evidence of each page of your Pension Credit documentation, upload one page at a time'},
+  'hc2': {documentType: 'hc2', title: 'Health Certificate (HC2) documentation', guidingText: 'You must provide evidence of your Health Certificate (HC2) documentation'},
+  'hc3': {documentType: 'hc3', title: 'Health Certificate (HC3) documentation', guidingText: 'You must provide evidence of your Health Certificate (HC3) documentation'},
+  'nhs-tax-credit': {documentType: 'nhs-tax-credit', title: 'NHS Tax credit documentation', guidingText: 'You must provide evidence of your NHS Tax credit documentation'},
+  'RECEIPT': {documentType: 'RECEIPT', title: 'Add receipt', guidingText: 'Provide a receipt or a copy of your tickets.'}
+}
+
+router.get('/ci/v1/upload', function (req, res) {
+  res.render('ci/v1/apply-upload', {
+    document: 'VISIT_CONFIRMATION',
+    fileUploadGuidingText: documentTypeEnum,
+    URL: req.url,
+    hideAlternative: req.query.hideAlt
+  })
+})
+
+router.post('/ci/v1/upload', function (req, res) {
+  res.render('index')
+})
+
 module.exports = router
